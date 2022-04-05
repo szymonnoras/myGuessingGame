6201f7702cf6165734d5e9d2446bf94fc8a1154b
szymonnoras <saronomysz@o2.pl>
2022-04-05 17:45:14 +0200

    Initial commit

---
+++ src/main/java/com/example/myguessinggame/MyGuessingGameApplication.java
@@ -0,0 +1,14 @@
+package com.example.myguessinggame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGuessingGameApplication {
+
+    public static void main(String[] args) {
+        System.out.println("dziala");
+        SpringApplication.run(MyGuessingGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myguessinggame/MyGuessingGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myguessinggame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGuessingGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
